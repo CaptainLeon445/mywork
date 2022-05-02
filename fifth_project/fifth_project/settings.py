42cb64b5f00c2c69d7c453f0af2e49c2da75b7b8
CaptainLeon445 <104680796+CaptainLeon445@users.noreply.github.com>
2022-05-02 08:58:18 +0100

    Update settings.py

--- fifth_project/fifth_project/settings.py
+++ fifth_project/fifth_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-asp%joh4pomst8@jl1$+xc!0qys08b+_r-ahca)c%7n&^x(e#u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['captainafrik.pythonanywhere.com']
 
 
 # Application definition
